b1fd3fd3788d383d8bf3fe2fd767900d83e14401
Alejandro Piña <alejjose8@gmail.com>
2024-10-14 02:49:27 -0300

    [fix] whatsapp button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,6 @@ import Incubator from "../components/home/incubator";
 import Coaching from "../components/home/coaching";
 import teamImg from "../images/home/team.png";
 import officeImg from "../images/home/office.png";
-import whatsapp from "../images/logos/whatsapp-logo-button.png";
 import Clients from "../components/home/clients";
 import Welcome from "../components/home/welcome";
 import { motion } from "framer-motion";
@@ -40,14 +39,6 @@ const IndexPage: React.FC<PageProps> = () => {
         </div>
         <Coaching />
         <Clients />
-        <div className="hidden md:block fixed bottom-0 right-0 -translate-x-1/2 -translate-y-1/2 z-10">
-          <a
-            href="https://api.whatsapp.com/send?phone=5215639308664&text=%C2%A1Hola!%20Me%20interesa%20saber%20m%C3%A1s%20de%20Zapian."
-            target="_blank"
-          >
-            <img src={whatsapp} alt="Whatsapp" />
-          </a>
-        </div>
       </div>
     </motion.div>
   );
